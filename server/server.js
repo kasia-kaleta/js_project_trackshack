e8c76f3a37568f0defd3ca5ccabe79444a83d6e0
gavinblack87 <gavin_black@hotmail.co.uk>
2019-08-19 10:15:10 +0100

    uncommented postFood

--- server/server.js
+++ server/server.js
@@ -19,30 +19,30 @@ MongoClient.connect('mongodb://localhost:27017')
   })
   .catch(console.error);
 //fetch
-  // app.post('/search', (req, res) => {
-  //   const query = req.body;
-  //   fetch("https://trackapi.nutritionix.com/v2/natural/nutrients", {
-  //     method: 'POST',
-  //     body: JSON.stringify(query),
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "x-app-id" : process.env.API_ID,
-  //       "x-app-key" : process.env.API_KEY
-  //     }
-  //   })
-  //   .then(res => res.json())//original response into json
-  //   .then(data => {//return, still as json, and return it to the frontend
-  //     res.json(data);
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //     res.status(500);
-  //     res.json({
-  //       status: 500,
-  //       error: err
-  //     });
-  //   });
-  // })
+  app.post('/search', (req, res) => {
+    const query = req.body;
+    fetch("https://trackapi.nutritionix.com/v2/natural/nutrients", {
+      method: 'POST',
+      body: JSON.stringify(query),
+      headers: {
+        "Content-Type": "application/json",
+        "x-app-id" : process.env.API_ID,
+        "x-app-key" : process.env.API_KEY
+      }
+    })
+    .then(res => res.json())//original response into json
+    .then(data => {//return, still as json, and return it to the frontend
+      res.json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({
+        status: 500,
+        error: err
+      });
+    });
+  })
 
 app.listen(3000, function () {
   console.log(`Listening on port ${ this.address().port }`);
